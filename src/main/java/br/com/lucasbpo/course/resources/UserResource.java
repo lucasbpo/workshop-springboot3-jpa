7f31af81121ef29c76f310cb84f97fb5150962c8
lucasbpo <lucasbpo@outlook.com.br>
2025-06-18 08:27:03 -0300

    feat: add `insert` methods for `User`.

--- src/main/java/br/com/lucasbpo/course/resources/UserResource.java
+++ src/main/java/br/com/lucasbpo/course/resources/UserResource.java
@@ -5,8 +5,11 @@ import br.com.lucasbpo.course.services.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.List;
 
@@ -30,5 +33,16 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         var user = service.findById(id);
         return ResponseEntity.ok(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        var user = service.insert(obj);
+        var location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(user);
     }
 }
